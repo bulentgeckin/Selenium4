e9636362eb03d2d96f9f4c2254a8975fecb39376
mehmetemmidev <mehmetemmidev@gmail>
2022-09-11 01:55:16 -0400

    cucumber runner for mvn test

--- src/test/java/features/TestBrowsers.feature
+++ src/test/java/features/TestBrowsers.feature
@@ -1,4 +1,3 @@
-@test
 Feature: Test Browsers
 
 
---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,22 @@
+package runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features =  {"src/test/java/features"}
+		,glue = "steps"
+		,monochrome = false
+//		,tags = "@browser"
+//		,tags = "@dataTableDemo"
+		,dryRun = false
+		,plugin = {"pretty", "html:target/html/cucumber-default-report.html", "json:target/cucumber.json", "rerun:target/failed.txt"}
+)
+
+public class TestRunner {}
